082402844bcce50f819345f487a6487cd17557b2
chadcarlson <chad.carlson@platform.sh>
2021-11-05 16:33:28 -0400

    Revert to push.

--- dodo.py
+++ dodo.py
@@ -131,18 +131,6 @@ def task_push():
             'actions': project.push,
         }
 
-def task_create_pull_request():
-    """
-    DoIt Task: Creates a pull request from the prepared branch on GitHub.
-
-    Usage: doit create_pull_request:<project>
-    """
-    for project in ALL_PROJECTS:
-        yield {
-            'name': project.name,
-            'actions': project.create_pull_request,
-        }
-
 def task_rebuild():
     """
     DoIt Task: Aggregates the update, platformify, and branch tasks for one-stop shopping.
