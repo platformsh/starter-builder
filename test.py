26caaf523d582cc24c8f41a38c208c57bed9a8ce
chadcarlson <chad.carlson@platform.sh>
2021-11-08 12:22:10 -0500

    Handle basic.

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-import dodo; print(dodo.project_factory('drupal9').remote)
+import dodo; import os; try: print(dodo.project_factory(os.environ["TEMPLATE"]).remote); except: print('None')
+
+
+python -c '
+import os
+import dodo
+try: 
+    print(dodo.project_factory(os.environ["TEMPLATE"]).remote)
+except:
+    print("None")
+'
