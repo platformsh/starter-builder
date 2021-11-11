59068f35aec132225b751ef434505c010761e47a
chadcarlson <chad.carlson@platform.sh>
2021-11-11 09:14:35 -0500

    passed envs.

--- wait_for_activity_complete.sh
+++ wait_for_activity_complete.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-PROJECT_ID=${{ steps.projectid.outputs.result }}
-ACTIVITY_ID=${{ steps.getactivity.outputs.id }}
-
+echo "Waiting for activity ($3) to complete on $1/$2"
 ACTIVITY_STATE="in_progress"
 until [ $ACTIVITY_STATE == "complete" ];do
     sleep 10
-    ACTIVITY_STATE=$(~/.platformsh/bin/platform project:curl -p $PROJECT_ID environments/$UPDATE_ENVIRONMENT/activities/$ACTIVITY_ID | jq -r '.state')
+    ACTIVITY_STATE=$(~/.platformsh/bin/platform project:curl -p $1 environments/$2/activities/$3 | jq -r '.state')
     echo "Activity state: $ACTIVITY_STATE"
 done
